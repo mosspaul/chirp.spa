adc1a8cd01930fbdf8bfa6830afe0935752c2490
mosspaul <gmosspaul18@gmail.com>
2026-06-13 11:29:43 -0600

    add sidebar and other routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,12 @@ import { Login } from './components/login/login';
 import { SignUp } from './components/sign-up/sign-up';
 import { authGuard } from './auth/route-guard';
 import { Dashboard } from './components/dashboard/dashboard';
+import { Profile } from './components/profile/profile';
+import { Settings } from './components/settings/settings';
+import { Accounts } from './components/accounts/accounts';
+import { Analytics } from './components/analytics/analytics';
+import { Transactions } from './components/transactions/transactions';
+
 
 export const routes: Routes = [
     {path: 'login', component: Login},
@@ -13,8 +19,13 @@ export const routes: Routes = [
         canActivate: [authGuard],
         component: Home,
         children: [ 
-            {path: 'dashboard', component: Dashboard},
-            { path: '', redirectTo: 'dashboard', pathMatch: 'full' }
+            { path: 'dashboard', component: Dashboard },
+            { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+            { path: 'profile', component: Profile },
+            { path: 'settings', component: Settings },
+            { path: 'accounts', component: Accounts },
+            { path: 'analytics', component: Analytics },
+            { path: 'transactions', component: Transactions }
         ]
     },
     {path: '**', redirectTo: '/'}
--- src/app/components/sidebar/sidebar.ts
+++ src/app/components/sidebar/sidebar.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { LucideAngularModule, LayoutDashboard, ArrowLeftRight, Wallet, PieChart, Bell, Settings, Layers } from 'lucide-angular';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive, LucideAngularModule],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
 })
-export class Sidebar {}
+export class Sidebar {
+  readonly icons = {
+    LayoutDashboard,
+    ArrowLeftRight,
+    Wallet,
+    PieChart,
+    Bell,
+    Settings,
+    Layers,
+  };
+}
